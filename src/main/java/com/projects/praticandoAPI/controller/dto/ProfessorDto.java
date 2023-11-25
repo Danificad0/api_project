f6dd94466a53625e40df80f55c84dc58e9257efa
Daniel Souza <danielxhalo2@hotmail.com>
2023-11-25 17:17:06 -0300

    3 Classes Criadas

---
+++ src/main/java/com/projects/praticandoAPI/controller/dto/ProfessorDto.java
@@ -0,0 +1,20 @@
+package com.projects.praticandoAPI.controller.dto;
+
+public class ProfessorDto {
+	private String nome;
+	private String curso;
+ 
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+}
